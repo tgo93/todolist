7c71c906f56694b0e74947c8880b641146d212e3
Travis <43999341+tgo93@users.noreply.github.com>
2021-02-01 11:45:28 -0400

    'Delete': validation issue resolved
    Logic now actually fixes issue with 0 index being deleted from to do list.

---
+++ todos.js
@@ -0,0 +1,32 @@
+const toDoList = [];
+let command = prompt("What do you want to do?");
+
+while (command !== "quit" && command !== 'q') {
+    if (command === 'new') {
+        let newToDo = prompt('Enter a new to-do:');
+        toDoList.push(newToDo);
+        console.log(`${newToDo} added to list.`);
+    } else if (command === 'list') {
+        console.log('**********');
+        for (let i = 0; i < toDoList.length; i++) {
+            console.log(`${i}: ${toDoList[i]}`)
+        }
+        console.log('**********');
+    } else if (command === 'delete') {
+        let toDont = parseInt(prompt('Enter index of item to delete:'));
+
+        // Ensures requested deletion exists
+        while (toDont >= toDoList.length || !toDont) {
+            toDont = parseInt(prompt('Enter index of item to delete:'))
+        }
+
+        // Only logs if successful
+        if (toDont || toDont === 0) {
+            console.log(`${toDoList[toDont]} removed from list.`);
+            toDoList.splice(toDont, 1);
+        }
+    }
+    command = prompt('What do you want to do?:');
+}
+
+console.log("Quitting...");
