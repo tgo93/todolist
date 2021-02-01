3d2a0b53406665d69f4d58d5649a4809d3fa51cf
Travis <43999341+tgo93@users.noreply.github.com>
2021-02-01 09:50:21 -0400

    Add files via upload
    First commit. Basic functionality of browser-based to do list app.

---
+++ ToDo/todos.js
@@ -0,0 +1,24 @@
+const toDoList = [];
+
+while (true) {
+    let command = prompt('What do you want to do?:');
+
+    if (command.toLowerCase() === 'quit' || command.toLowerCase() === 'q') {
+        console.log('Quitting...');
+        break;
+    } else if (command === 'new') {
+        let newToDo = prompt('Enter a new to-do:');
+        toDoList.push(newToDo);
+        console.log(`${newToDo} added to list.`);
+    } else if (command === 'list') {
+        console.log('**********');
+        for (let i = 0; i < toDoList.length; i++) {
+            console.log(`${i}: ${toDoList[i]}`)
+        }
+        console.log('**********');
+    } else if (command === 'delete') {
+        let toDont = parseInt(prompt('Enter index of item to delete:'));
+        console.log(`${toDoList[toDont]} removed from list.`);
+        toDoList.splice(toDont, 1);
+    }
+}
